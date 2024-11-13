a8c8d3829e6cedcd64909171459109d2587306ee
AnnaVoroshnina <bologova.ann@yandex.ru>
2024-11-13 21:44:09 +0300

    fixed modal window

--- index.js
+++ index.js
@@ -6,16 +6,12 @@ const modalWindow = document.getElementById("overlay")
 const createTaskBtn = document.getElementById("openModal")
 const closeModalBtn = document.getElementById("close-modal")
 const closeModalBtnMobile = document.getElementById("close-modal-mobile")
+const closeModalBtnMobileDown = document.getElementById("close-modal-down")
 
 sidebarTab.addEventListener("click", function() {
     this.classList.toggle("active");
     sidebarBody.classList.toggle("visually-hidden")
-    this.classList.contains("active") ? content.style.marginLeft= "0px" : content.style.marginLeft = "56px"
 });
-
-if (window.innerWidth < 767) {
-    content.style.marginLeft= "0px"
-}
 
 sidebarStar.addEventListener("click", function() {
     this.classList.toggle("active");});
@@ -37,7 +33,10 @@ modalWindow.addEventListener("click", function(e) {
 closeModalBtn.addEventListener("click", function() {
         modalWindow.close()
 })
-closeModalBtnMobile.addEventListener("click", function(e) {
+closeModalBtnMobile.addEventListener("click", function() {
+    modalWindow.close()
+})
+closeModalBtnMobileDown.addEventListener("click", function() {
     modalWindow.close()
 })
 
